a281232397de468120f4d3ed64418e4734c06507
Adriano Goulart <44497896+adrianodg@users.noreply.github.com>
2019-12-08 15:22:57 -0300

    Update banco de dados

--- src/lojaCalcados.sql
+++ src/lojaCalcados.sql
@@ -7,39 +7,42 @@ USE lojaCalcados ;
 -- -----------------------------------------------------
 -- Table Fornecedor
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS fornecedores (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Fornecedor (
+  idFornecedor INT NOT NULL AUTO_INCREMENT,
   nomeFornecedor VARCHAR(45) NOT NULL,
   cnpj CHAR(18) NOT NULL,
   telFornecedor VARCHAR(15) NOT NULL,
   emailFornecedor VARCHAR(100) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX cnpj_UNIQUE (cnpj));
+  PRIMARY KEY (idFornecedor),
+  UNIQUE INDEX cnpj_UNIQUE (cnpj ASC))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table Produto
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS produtos (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Produto (
+  idProduto INT NOT NULL AUTO_INCREMENT,
   nomeProduto VARCHAR(60) NOT NULL,
   precoFinal FLOAT NOT NULL,
   precoCusto FLOAT NOT NULL,
-  fornecedores_id INT NOT NULL,
-  PRIMARY KEY (id, fornecedores_id),
-  UNIQUE INDEX nomeProduto_UNIQUE (nomeProduto),
-  INDEX produto_idx (fornecedores_id),
-    FOREIGN KEY (fornecedores_id)
-    REFERENCES fornecedores (id)
-     ON DELETE NO ACTION
-     ON UPDATE CASCADE);
+  idFornecedor INT NOT NULL,
+  PRIMARY KEY (idProduto, idFornecedor),
+  UNIQUE INDEX nomeProduto_UNIQUE (nomeProduto ASC),
+  INDEX fk_Estoque_Fornecedor1_idx (idFornecedor ASC),
+  CONSTRAINT fk_Estoque_Fornecedor1
+    FOREIGN KEY (idFornecedor)
+    REFERENCES Fornecedor (idFornecedor)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table Usuario
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS usuarios (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Usuario (
+  idUsuario INT NOT NULL AUTO_INCREMENT,
   nomeUsuario VARCHAR(60) NOT NULL,
   cpf CHAR(14) NOT NULL,
   telefoneUsuario VARCHAR(15) NOT NULL,
@@ -47,48 +50,48 @@ CREATE TABLE IF NOT EXISTS usuarios (
   enderecoUsuario VARCHAR(200) NOT NULL,
   loginUsuario VARCHAR(100) NOT NULL,
   senhaUsuario VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id),
-  UNIQUE INDEX cpf_UNIQUE (cpf));
-
+  PRIMARY KEY (idUsuario),
+  UNIQUE INDEX cpf_UNIQUE (cpf ASC))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table Compra
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS compras (
-  id INT NOT NULL AUTO_INCREMENT,
-  produtos_id INT NOT NULL,
-  usuarios_id INT NOT NULL,
+CREATE TABLE IF NOT EXISTS Compra (
+  idCompra INT NOT NULL AUTO_INCREMENT,
+  idProduto INT NOT NULL,
+  idUsuario INT NOT NULL,
   formaPagamento TINYINT NOT NULL,
   qtdeCompra INT NOT NULL,
-  PRIMARY KEY (id, produtos_id, usuarios_id),
-  INDEX compra_us_idx (usuarios_id),
-  INDEX compra_prod_idx (produtos_id),
-    FOREIGN KEY (produtos_id)
-    REFERENCES produtos (id)
+  PRIMARY KEY (idCompra, idProduto, idUsuario),
+  INDEX fk_Produtos_has_Clientes_Clientes1_idx (idUsuario ASC),
+  INDEX fk_Produtos_has_Clientes_Produtos_idx (idProduto ASC),
+  CONSTRAINT fk_Produtos_has_Clientes_Produtos
+    FOREIGN KEY (idProduto)
+    REFERENCES Produto (idProduto)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-    FOREIGN KEY (usuarios_id)
-    REFERENCES usuarios (id)
+  CONSTRAINT fk_Produtos_has_Clientes_Clientes1
+    FOREIGN KEY (idUsuario)
+    REFERENCES Usuario (idUsuario)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table Estoque
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS estoques (
-  produtos_id INT NOT NULL,
-  fornecedores_id INT NOT NULL,
-  id TINYINT(2) NOT NULL,
+CREATE TABLE IF NOT EXISTS Estoque (
+  idProduto INT NOT NULL,
+  idFornecedor INT NOT NULL,
+  tamanhoProduto TINYINT(2) NOT NULL,
   qtdeProduto INT NULL,
-  PRIMARY KEY (produtos_id, fornecedores_id, id),
-    FOREIGN KEY (produtos_id)
-    REFERENCES produtos (id),
-    FOREIGN KEY (fornecedores_id)
-    REFERENCES fornecedores (id)
+  PRIMARY KEY (idProduto, idFornecedor, tamanhoProduto),
+  CONSTRAINT fk_table1_Produto1
+    FOREIGN KEY (idProduto , idFornecedor)
+    REFERENCES Produto (idProduto , idFornecedor)
     ON DELETE CASCADE
-    ON UPDATE CASCADE);
-
-
-
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
