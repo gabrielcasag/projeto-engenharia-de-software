a5d3ab9affef6bf14766122879d19ababd5777ab
gabrielcasag <gabriel.casagrande@estudante.ufla.br>
2019-12-09 09:28:15 -0300

    Update controller

--- src/Padrao MVC/Controller/BuscarProduto.php
+++ src/Padrao MVC/Controller/BuscarProduto.php
@@ -12,5 +12,65 @@
 
     $res = $produtoDAO->buscar();
 
+    if ($res->num_rows > 0 )
+    {
+        echo 
+        "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='../CSS/cadastrarProduto.css'>
+            <meta charset='utf-8'>
+            <title>TripleG Store</title>
+        </head>
+        <center>
+        <body>
+            <div class='frame' >
+                <h1>Produto Encontrado</h1>
+                <br>
+                <label for='text'>
+                    ID : " .$res['idProduto'] . "
+                </label>
+                <label for='nome'>
+                    Nome: " .$res['nomeProduto'] ."
+                </label>
+                <label for='precofinal'>
+                    Preço Final: " .$res['precoFinal'] ."
+                </label>
+                <label for='precocusto'>
+                    Preço Custo: " .$res['precoCusto'] ."
+                </label>
+                <label for='id'>
+                    ID do Fornecedor: " .$res['idFornecedor'] ."
+                </label>
+            </div>
+        </body>
+        </center>	
+        </html>
+        "
+    }
+    else
+    {
+        echo 
+        "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='../CSS/cadastrarProduto.css'>
+            <meta charset='utf-8'>
+            <title>TripleG Store</title>
+        </head>
+        <center>
+        <body>
+            <div class='frame'>
+                <br>
+                <h1>Produto Nao Encontrado !!</h1>
+            </div>
+        </body>
+        </center>
+        </html>
+        "
+    }
+    
     header('Location: ../index.php');
 ?>
