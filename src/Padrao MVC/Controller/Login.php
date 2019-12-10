336b438261b0a849c81b1558b6e4ade4d4b14c99
gabrielcasag <gabriel.casagrande@estudante.ufla.br>
2019-12-10 19:05:59 -0300

    Update Login.php
    Closes #14

--- src/Padrao MVC/Controller/Login.php
+++ src/Padrao MVC/Controller/Login.php
@@ -3,13 +3,13 @@
     include_once("../Persistence/connection.php");
 
     $conexao = new connection();
-    $conexao->getCon();
+    $link = $conexao->getCon();
 
     $login = $_POST['clogin'];
     $senha = $_POST['csenha'];
 
     $logindao = new LoginDAO();
-    $resultado = $logindao->validar($login,$senha,$conexao);
+    $resultado = $logindao->validar($login,$senha,$link);
 
     if ( $resultado->num_rows > 0 )
     {
@@ -22,7 +22,7 @@
         <!DOCTYPE html>
         <html>
         <head>
-            <link rel='stylesheet' href='../CSS/cadastrarProduto.css'>
+            <link rel='stylesheet' href='../CSS/baseform.css'>
             <meta charset='utf-8'>
             <title>TripleG Store</title>
         </head>
@@ -30,11 +30,17 @@
         <body>
             <div class='frame'>
                 <br>
-                <h1>Produto Nao Encontrado !!</h1>
+                <h1>Login inválido !!</h1>
+                <br><br>
+                <label for='voltar'>
+                <button style='width:70px;height:30px' onclick='location.href = "."\"../View/index.html\";'  class='float-left submit-button'>Voltar</button>
+                </label>
             </div>
+            <center>
+
         </body>
         </center>
         </html>
         ";
-
     }
+?>
